acaa4a1f548043a570fb23f17d8fae093cede21b
Henry Ripple <hripple46@gmail.com>
2023-04-30 17:07:21 -0500

    connected app to MongoDB and hosting on Fly.io

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,16 @@
 var express = require("express");
 var router = express.Router();
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://hripple46:9dLjkWYCta_nncN@cluster0.xe9kaoh.mongodb.net/?retryWrites=true&w=majority"
+);
+
+const messageSchema = new mongoose.Schema({
+  text: String,
+  user: String,
+  added: Date,
+});
+const userMessage = mongoose.model("userMessage", messageSchema);
 
 const messages = [
   {
@@ -28,6 +39,13 @@ router.post("/new", function (req, res, next) {
     user: req.body.user,
     added: new Date(),
   });
+  let message1 = new userMessage({
+    text: req.body.newMessage,
+    user: req.body.user,
+    added: new Date(),
+  });
+  message1.save();
+
   res.redirect("/");
 });
 
